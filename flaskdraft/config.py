8d115f85df385a6251d9c8ae4df2fe5fd58987f5
mvantschip <54024067+mvantschip@users.noreply.github.com>
2020-05-26 16:20:47 +0200

    postgresql

--- flaskdraft/config.py
+++ flaskdraft/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost:5432/draft"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = os.urandom(32)
