9f1ab95718caac1e2e8aee201b4849411ffd4b25
mvantschip <54024067+mvantschip@users.noreply.github.com>
2020-05-26 16:53:40 +0200

    postgresql 3

--- flaskdraft/config.py
+++ flaskdraft/config.py
@@ -4,6 +4,6 @@ import psycopg2
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URI']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = os.urandom(32)
