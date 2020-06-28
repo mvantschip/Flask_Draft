3fd9da24ca7a2710432875d7e1ef90dde9f3df66
mvantschip <54024067+mvantschip@users.noreply.github.com>
2020-06-28 21:10:57 +0200

    team name change

--- flaskdraft/bids/forms.py
+++ flaskdraft/bids/forms.py
@@ -7,7 +7,7 @@ class PlaceBids(FlaskForm):
     bid_player_value = DecimalField('Bod in miljoenen', places = 2, validators = [NumberRange(min=1, max=250), DataRequired()])
     username = SelectField('Teamnaam', choices = sorted([('', 'Selecteer uw teamnaam'), ('GSV Niedermeier', 'GSV Niedermeier'), ('Micronesia Maniacs', 'Micronesia Maniacs'),
     ('Adrianus Albertus \'t Vijfje', 'Adrianus Albertus \'t Vijfje'), ('FC SJHEV', 'FC SJHEV'), ('FC Groen Goud', 'FC Groen Goud'),
-    ('Los Banditos', 'Los Banditos'), ('v.v. Twenthe 7', 'v.v. Twenthe 7'), ('FC Kuitkramp', 'FC Kuitkramp'), ('Boca Seniors', 'Boca Seniors'),
+    ('Los Banditos', 'Los Banditos'), ('v.v. Twenthe 7', 'v.v. Twenthe 7'), ('FC Kuitkramp', 'FC Kuitkramp'), ('Mocro\'s Mannen', 'Mocro\'s Mannen'),
     ('Newton Heath', 'Newton Heath')]), validators = [DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Bieden')
--- flaskdraft/users/forms.py
+++ flaskdraft/users/forms.py
@@ -8,7 +8,7 @@ class RegistrationForm(FlaskForm):
     ('Adrianus Albertus \'t Vijfje', 'Adrianus Albertus \'t Vijfje'), ('FC SJHEV', 'FC SJHEV'),
     ('FC Groen Goud', 'FC Groen Goud'), ('Los Banditos', 'Los Banditos'),
     ('v.v. Twenthe 7', 'v.v. Twenthe 7'), ('FC Kuitkramp', 'FC Kuitkramp'),
-    ('Boca Seniors', 'Boca Seniors'), ('Newton Heath', 'Newton Heath')]), validators = [DataRequired()])
+    ('Mocro\'s Mannen', 'Mocro\'s Mannen'), ('Newton Heath', 'Newton Heath')]), validators = [DataRequired()])
     registration_email = StringField('E-mailadres', validators=[DataRequired(), Email()])
     registration_password = PasswordField('Wachtwoord', validators=[DataRequired()])
     registration_confirm_password = PasswordField('Bevestig wachtwoord', validators=[DataRequired(), EqualTo('registration_password')])
